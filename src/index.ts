60fe6221d5f85cefe126e413dd00cbade753bfe8
caxxy <yxx4c@users.noreply.github.com>
2024-01-19 09:34:21 +0300

    fix(type) export option types to avoid type error ts(4023)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 export {getCacheKey} from './getCacheKey';
 export {getCacheKeyPattern} from './getCacheKeyPattern';
 export {PrismaRedisExtension} from './prismaRedisExtension';
-export type {AutoCacheConfig, CacheConfig} from './types';
+export type {
+  AutoCacheConfig,
+  CacheConfig,
+  CacheOptions,
+  UncacheOptions,
+} from './types';
 export {filterOperations, unlinkPatterns} from './utils';
